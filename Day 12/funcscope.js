968b07e04f3b58be17efe60b51b43ce7833cd6a7
Prasath S <prasath.sadhasivam@i-exceed.com>
2024-05-21 17:39:29 +0530

    Add files via upload

---
+++ Day 12/funcscope.js
@@ -0,0 +1,17 @@
+'use strict';
+let model="petrol";
+if(model=="petrol")
+{
+    var check=function()
+    {
+        console.log("It is a Petrol car");
+    };
+}
+else
+{
+    var check=function()
+    {
+        console.log("It is a Diesel car");
+    };
+}
+check();
