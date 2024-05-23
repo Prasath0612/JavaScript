c6a2f9f31211c846ff2660df5623df9e38d10e07
Prasath S <prasath.sadhasivam@i-exceed.com>
2024-05-23 18:22:34 +0530

    Add files via upload

---
+++ Day 14/objEntries.js
@@ -0,0 +1,10 @@
+const debitCard={
+    fname:"Kiran",
+    lname:"Singh",
+    panNo:"WDFSG6754A",
+    valid:"7years"
+}
+let c=Object.entries(debitCard);
+console.log(c);
+
+console.log(Object.fromEntries(c));
