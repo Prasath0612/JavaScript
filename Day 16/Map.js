ae76bf6d2cebf58c2e539d3929ea336c948a76ee
Prasath S <prasath.sadhasivam@i-exceed.com>
2024-05-27 17:43:56 +0530

    Add files via upload

---
+++ Day 16/Map.js
@@ -0,0 +1,25 @@
+// const form=new Map();
+// //setting the values for map using set()
+// form.set("Name","Shankar");
+// form.set("DOB","20/3/02");
+// form.set("Age",22);
+// form.set("E-mail","shankarabc@gmail.com");
+// form.set("locPreference",["Chennai","Cbe","Salem"]);
+// //assigning map1 to map2
+// const form1=form;
+// //getting the values of map using get()
+// //console.log("Age:"+form1.get("Age"));
+// form1.forEach((items,index)=>{
+//     console.log(`${index} : ${items}`);
+// })
+// const loc=form.get("locPreference");
+// console.log(loc[2]);
+// console.log(form.has("Name"));
+// console.log(form.keys())
+// console.log(form.values())
+// console.log(form.delete("Age"))
+// console.log(form.keys())
+
+const s=new Set(["a","b","c"]);
+s.forEach((x)=>console.log(x));
+console.log(s);
